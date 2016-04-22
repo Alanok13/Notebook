7e7fa60fa67cf8d2353e967574a33f4430b6bfbb
Alanok13 <alan@xaker.ru>
2016-04-22 15:38:18 +0300

    applicationContext and database.properties moved to resources.

--- src/main/java/uz/alano/notebook/dao/DefaultContactsDAO.java
+++ src/main/java/uz/alano/notebook/dao/DefaultContactsDAO.java
@@ -3,14 +3,14 @@ package uz.alano.notebook.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import uz.alano.notebook.model.Contact;
 
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Repository
 public class DefaultContactsDAO implements ContactsDAO {
 
     @Autowired
